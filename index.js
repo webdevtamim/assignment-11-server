ad26ea560fa2d6a084e77e8c9dbed06cd69ce2e7
webdevtamim <tamimahmed12.bd@gmail.com>
2023-11-08 13:30:19 +0600

    post operation for all jobs

--- index.js
+++ index.js
@@ -26,12 +26,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+    const jobCollection = client.db("jobsDB").collection("Jobs");
+
+    
+
+    app.post('/jobs', async (req, res) => {
+        const newJobs = req.body;
+        const result = await jobCollection.insertOne(newJobs);
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
